185dfe9f4bf3a498017dd8c0217aaaaf4ef32fee
Dlaize Kohli <dakshkohli23@gmail.com>
2021-05-19 16:34:59 +0530

    Update progress.py

--- progress.py
+++ progress.py
@@ -23,12 +23,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n\n⏳ Percentage: {2}%\n".format(
+        progress = "[{0}{1}] \n\n<b>⏳ Percentage:</b> {2}%\n".format(
             ''.join(["◆" for i in range(math.floor(percentage / 5))]),
             ''.join(["◇" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "<b>✔️ Completed:</b> {0} \n<b>🗄️ Total Size:</b> {1}\n<b>🚄 Speed:</b> {2}/s\n<b>🕰️ ETA:</b> {3}\n<b></b>\n\n<b>⌊` 💡 Dlaize `⌉</b>\n".format(
+        tmp = progress + "<b>☑️ Completed:</b> {0} \n<b>🗄️ Total Size:</b> {1}\n<b>🚄 Speed:</b> {2}/s\n<b>🕰️ ETA:</b> {3}\n<b></b>\n\n<b>⌊`♟️ Dlaize`⌉</b>\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
